99d1e5e979732298b02c2f7815b2fa49c7483b9f
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-05-19 15:37:56 +0900

    port change

--- main/up.sh
+++ main/up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker compose up -d
-echo "JOB SMP | http://localhost:9203/csp/sys/%25CSP.Portal.Home.zen?$NAMESPACE=JOB"
-echo "TASK SMP | http://localhost:9204/csp/sys/%25CSP.Portal.Home.zen?$NAMESPACE=TASK"
+echo "JOB SMP | http://localhost:52873/csp/sys/%25CSP.Portal.Home.zen?$NAMESPACE=JOB"
+echo "TASK SMP | http://localhost:52874/csp/sys/%25CSP.Portal.Home.zen?$NAMESPACE=TASK"
