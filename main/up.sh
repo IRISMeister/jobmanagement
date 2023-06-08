e9439a510a0f4ffee791cbdfa3c9088395b63052
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-06-08 18:05:55 +0900

    wip

--- main/up.sh
+++ main/up.sh
@@ -2,7 +2,9 @@
 
 docker compose up -d
 docker compose exec -u root job service ssh start
+sleep 1
 docker compose exec -u root task1 service ssh start
+sleep 1
 docker compose exec -u root task2 service ssh start
 echo "JOB SMP | http://localhost:52873/csp/sys/%25CSP.Portal.Home.zen?$NAMESPACE=JOB"
 echo "TASK SMP | http://localhost:52874/csp/sys/%25CSP.Portal.Home.zen?$NAMESPACE=TASK"
